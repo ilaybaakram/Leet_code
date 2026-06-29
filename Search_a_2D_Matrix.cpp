5194fef19018f42b153fd941b6154d5a10c43f58
Laiba Akram <laibaakram00@gmail.com>
2026-06-29 17:39:01 +0500

    Add files via upload

---
+++ Search_a_2D_Matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int s = 0;
+        int e = (m*n) - 1;
+
+        while(s <= e)
+        {
+            int mid = (s + e) / 2;
+
+            int rows = mid / n;
+            int cols = mid % n;
+
+            if(matrix[rows][cols] == target)
+            {
+                return true;
+                break;
+            }
+            else if(matrix[rows][cols] < target)
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+        return false;
+    }
+};
